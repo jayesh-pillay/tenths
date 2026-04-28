7b83401e78bc811cb6dbd249270ff3c5d3ef27e9
Jayesh Pillay <jayeshpillay2304@gmail.com>
2026-04-28 18:31:03 +0530

    Fix file paths for FrankenPHP

--- api/setup.php
+++ api/setup.php
@@ -10,7 +10,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'db.php';
+require_once __DIR__ . '/db.php';
 
 // Read the unified schema file from the parent directory
 $schema_path = __DIR__ . '/../production_schema.sql';
